11428b824c7e3325e3c80615432c7e3062d10bf2
Guillaume Coiffier <guillaume.coiffier.42@orange.fr>
2017-03-01 10:08:57 +0100

    a bit of cleanup

--- _build/main.ml
+++
@@ -1,8 +0,0 @@
-open Formula
-open Bdd
-open API
-
-let f1 = read_formula();;
-let f2 = AND( Var(1) , XOR( AND( Var(2), Var(3)), NOT(Var(4))));;
-let my_bdd = BDD.create f1;;
-BDD.print my_bdd "test";;
--- _build/tests.ml
+++
@@ -1,3 +0,0 @@
-open Formula
-
-let f1 = AND( Var(1) , XOR( AND( Var(2), Var(3)),Var(4)))
