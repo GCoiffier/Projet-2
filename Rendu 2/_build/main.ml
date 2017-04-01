473ccdbb4e1d989623892595242e872998b934a7
Guillaume Coiffier <guillaume.coiffier.42@orange.fr>
2017-04-01 13:08:13 +0200

    added environnement module

---
+++ Rendu 2/_build/main.ml
@@ -0,0 +1,16 @@
+(* -----------------------------------------
+  |    PROJET 2  2017 - L3IF ENS Lyon      |
+  |    Valque Léo - Coiffier Guillaume     |
+  |                                        |
+  |                 Rendu 2                |
+  |                Main file               |
+  ------------------------------------------
+
+  NB : Tout ce qu'il faut savoir est dans le README.md
+*)
+
+open Argv
+
+let _ = argv_call ();;
+
+(* let _ = argv_call ();; *)
---
+++ Rendu 2/_build/parser.mli
@@ -0,0 +1,32 @@
+type token =
+  | CONST of (int)
+  | VARIABLE of (string)
+  | ADD
+  | MINUS
+  | MULT
+  | DIV
+  | MOD
+  | AND
+  | OR
+  | NOT
+  | EGALE
+  | NEG
+  | SUPS
+  | INFS
+  | SUPE
+  | INFE
+  | IF
+  | THEN
+  | ELSE
+  | PRINT
+  | LET
+  | IN
+  | FUN
+  | IMPLIES
+  | LPAREN
+  | RPAREN
+  | EOL
+  | EINSTR
+
+val main :
+  (Lexing.lexbuf  -> token) -> Lexing.lexbuf -> Fouine.programme
