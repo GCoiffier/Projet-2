ef92be58ad7e3a4459708c112f03828c3e1cc06e
Guillaume Coiffier <guillaume.coiffier.42@orange.fr>
2017-04-05 10:23:48 +0200

    bla

---
+++ Rendu 2/_build/debug.ml
@@ -0,0 +1,37 @@
+open Environnement
+open Prog_type
+
+let rec debug = function
+    Const(n) -> print_int n
+    |Var(x) -> print_string x
+    |PrInt(a) -> print_string "prInt("; debug a ; print_string ")"
+    |IfThenElse(b,p1,p2) -> print_string "If (" ;
+                            debug b ;
+                            print_string ") Then (" ;
+                            debug p1 ;
+                            print_string ") Else (";
+                            debug p2;
+                            print_string ")"
+    |UnOp(op,a) -> print_string (match op with Neg -> "-(" | Not -> "!(");
+                   debug a;
+                   print_string ")"
+    |BinOp(a,op,b) -> print_string "(";
+                        debug a;
+                        print_string ( match op with
+                            | Add -> "+"     | Minus -> "-"  | And -> "&&"
+                            | Or -> "||"     | Mult -> "*"   | Div -> "/"
+                            | Mod -> " mod " | Equal -> "="  | Neq -> "<>"
+                            | Infeq -> "<="  | Inf -> "<"    | Supeq -> ">="
+                            | Sup -> ">");
+                        debug b
+    |Let(x,a,b) ->  print_string ("let "^x^" = (");
+                    debug a;
+                    print_string ") in (";
+                    debug b;
+                    print_string ")"
+    |Imp(a,b) -> debug a;
+                 print_string " ; ";
+                 debug b
+    |Function_def(x,a) -> print_string ("fun "^x^" -> ");
+                        debug a
+    | _ -> failwith "Work in progress"
