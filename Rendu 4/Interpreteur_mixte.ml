e81aded82cfb751baf80264a3b0017469454934a
Guillaume Coiffier <guillaume.coiffier.42@orange.fr>
2017-05-10 12:12:42 +0200

    fin de séance

--- Rendu 4/Interpreteur_mixte.ml
+++ Rendu 4/Interpreteur_mixte.ml
@@ -3,7 +3,6 @@ open Fouine_type
 open Machine
 
 type ret = Env.elt * bool (* Le type de retour de l'interprétation. Le booléen indique si on a levé une exception *)
-type mixed_programme = programme*bool (*  Le booléen indique la pureté du code. Il est calculé en avance *)
 
 let return = function
       Env.Int(n) -> n
@@ -13,41 +12,60 @@ let return = function
 
 
 let rec label_pure_code : programme -> mixed_programme = function
-    Unit -> Unit,true
-    | Const(n) as c -> c,true
-    | Var(x) as v -> v,true
-    | PrInt(p) as pr -> let v,b = label_pure_code p in pr,b
-    | Let(x,val_x,p) as lt-> let vx,bx = label_pure_code val_x and vp,bp = label_pure_code p in
-                                    lt, (bx&&bp)
-    | UnOp(op,a) as un ->  let v,b = label_pure_code a in un,b
-    | BinOp(a,op,b) as bin -> let va,ba = label_pure_code a and vb,bb = label_pure_code b in
-                                    bin, (ba&&bb)
-    | x -> x,false
+    Unit -> Pure(Const(0))
+    | Const(n) as c -> Pure(c)
 
+    | Var(x) as v -> Pure(v)
 
+    | PrInt(p) as prt-> let x = label_pure_code p in
+                        (match x with
+                        | Pure(_) -> Pure(prt)
+                        | _ -> PrInt(x))
 
-(* ------ Execution complète (avec les extensions) ------ *)
-let execute_mixte : programme -> int = fun prg ->
-    let rec exec_aux env  = function
+    | PrStr(s) as p -> Pure(p)
 
-    | x,pure when pure -> Env.Int(StackMachine.init_and_compute x),false
+    | PrNL -> Pure(PrNL)
 
-    | LetRec(f,expr_f,p),_ ->  let copy = Env.copy env in
-                            let clt = Env.Cloture(expr_f, copy) in
-                            Env.add copy f clt; (* Comme la fonction s'appelle elle-même, la cloture se contient elle même*)
-                            Env.add env f clt;
-                            let ret,b = (exec_aux env  p) in
-                            Env.remove env f; ret,b
+    | Let(x,val_x,p) as lt -> let rx = label_pure_code val_x and px = label_pure_code p in
+                                let resx = (match rx with Pure(_) as a -> a | _ -> resx Pure(lt)
 
-    | IfThenElse(b,p1,p2),_ -> let x,b = (exec_aux env b) in
-                             if b then x,b
-                             else if ((return x)=1) then (exec_aux env p1)
-                                        else (exec_aux env p2)
+    | LetRec(f,expr_f,p) as ltrec ->  Pure(ltrec)
 
+    | IfThenElse(b,p1,p2) -> Pure()
+
+    | UnOp(op,a) ->  let v,b = (exec_aux env a) in
+                    if b then v,b else
+                    let x = return v in
+                    ( match op with
+                         Neg -> Env.Int(-x)
+                       | Not -> Env.Int(1-x)
+                    ),false
+
+    | BinOp(a,op,b) -> let va,ba = (exec_aux env a) and
+                          vb,bb = (exec_aux env b) in
+                      if ba then va,true else
+                      if bb then vb,true else
+                      let xa = return va and xb = return vb in
+                      let c1 = Env.Int(1) and c0 = Env.Int(0) in
+                       ( match op with
+                          | Add -> Env.Int(xa+xb)
+                          | Minus -> Env.Int(xa - xb)
+                          | And -> Env.Int(xa*xb)
+                          | Or -> if ((xa+xb)>0) then c1 else c0
+                          | Mult -> Env.Int(xa*xb)
+                          | Div -> Env.Int(xa/xb)
+                          | Mod -> Env.Int(xa mod xb)
+                          | Equal -> if (xa==xb) then c1 else c0
+                          | Neq -> if (xa<>xb) then c1 else c0
+                          | Infeq -> if (xa<=xb) then c1 else c0
+                          | Inf -> if (xa<xb) then c1 else c0
+                          | Supeq -> if (xa>=xb) then c1 else c0
+                          | Sup -> if (xa>xb) then c1 else c0
+                       ),false
     (* Fonctions *)
-    | Function_def(_,_) as f,_ -> Env.Cloture(f, Env.copy env),false
+    | Function_def(_,_) as f -> Env.Cloture(f, Env.copy env),false
 
-    | Function_call(f,arg),_ ->   let value,b = exec_aux env arg in
+    | Function_call(f,arg) ->   let value,b = exec_aux env arg in
                                 if b then value,b else
                                 let v,b = (exec_aux env f) in
                                 if b then v,b else
@@ -60,58 +78,68 @@ let execute_mixte : programme -> int = fun prg ->
                                 | _ -> failwith "Error in function call"
                                 )
     (* Exceptions  *)
-    | TryWith(p1,x,p2),_ -> let old_env = Env.copy env in
-                            let v,b = (exec_aux env p1) in
-                                Env.add old_env x v;
-                                if b then (exec_aux old_env p2)
-                                     else v,false
+    | TryWith(p1,x,p2) -> TryWith(label_pure_code p1, label_pure_code x, label_pure_code p2)
 
-    | Raise(expr),_ -> (fst (exec_aux env expr)),true
+    | Raise(expr) -> Raise(label_pure_code expr)
 
     (* aspects impératifs et références *)
-    | Imp(p1,p2),_ -> let u,b = exec_aux env p1 in
-                        (* test pour vérifier que l'on a pas de raise dans p1, auquel cas il ne faut PAS executer p2 *)
-                        if b then u,b else
-                            (exec_aux env p2)
+    | Imp(p1,p2) -> Imp(label_pure_code p1, label_pure_code p2)
+    | Ref(x) -> Ref(label_pure_code x)
+    | Bang(x) -> Bang(label_pure_code x)
+    | Assign(x,p) -> Assign(label_pure_code x, label_pure_code p)
 
-    | Ref(x),_ -> let v,b = (exec_aux env x) in
-                    if b then v,b else
-                        Env.Ref(ref (return v)),false
-
-    | Bang(x),_ -> let u = (Env.find env x) in
-                    (match u with
-                        Env.Ref(a) -> Env.Int(!a),false
-                        | _ -> failwith "Error : Not a reference"
-                    )
-
-    | Assign(x,p),_ -> let u,b = (exec_aux env p) in
-                        if b then u,b else begin
-                         (match (Env.find env x) with
-                            Env.Ref(r) -> r := (return u)
-                            | _ -> failwith "Error : Not a reference");
-                        u,false end
     (* tableaux  *)
-    | AMake(x),_ -> let n,b = (exec_aux env x) in
-                        if b then n,b else (Env.Array (Array.make (return n) 0)),false
+    | AMake(x) -> AMake(label_pure_code x)
+    | Affect(t,i,x) -> Affect( label_pure_code t, label_pure_code i, label_pure_code x)
+    | Access(t,i) -> Acces(label_pure_code t, label_pure_code i)
 
-    | Affect(t,i,x),_ -> let ind,b = (exec_aux env i) in
-                            if b then ind,b else
-                        let tab = Env.find env t in
-                            (match tab with
-                                Env.Array(ar) -> let ret,b = exec_aux env x in
-                                                    if b then ret,b else begin
-                                                        ar.((return ind)) <- (return ret);
-                                                        ret,false end
-                                | _ -> failwith "Error with affect : not an array"
-                            )
 
-    | Access(t,i),_ -> let ind,b = (exec_aux env i) in
-                    if b then ind,b else
-                        let tab = Env.find env t in
-                            (match tab with
-                                Env.Array(ar) -> Env.Int(ar.(return ind)),false
-                                | _ -> failwith "Error with access : not an array"
-                            )
+
+(* ------ Execution complète (avec les extensions) ------ *)
+let execute_mixte : programme -> int = fun prg ->
+    let rec exec_aux env  = function
+    Unit -> Env.Int(0),false
+
+    | Const(n,pure) -> Env.Int(n),false
+
+    | Var(x,pure) ->
+
+    | PrInt(x,pure) -> let ret,exc = if pure then (StackMachine.init_and_compute x),false
+                                    else (exec_aux env p)
+                    in(match ret with
+                       Env.Int(x) -> print_int x; print_newline ()
+                       |_ -> failwith "Execution Error in prInt"
+                    ) ; ret,exc
+
+    | PrStr(x,pure) -> print_string s ; Env.Int(0),false
+
+	| PrNL -> print_newline(); Env.Int(0),false
+
+    | UnOp(op,a,pure) ->  let v,b =  if pure then (StackMachine.init_and_compute a),false
+                                             else (exec_aux env a) in
+                    if b then v,b else
+                    let x = return v in
+                    ( match op with
+                         Neg -> Env.Int(-x)
+                       | Not -> Env.Int(1-x)
+                    ),false
+
+    | BinOp of mixed_programme * binary_op * mixed_programme * bool
+    | Let of mixed_programme  * mixed_programme * mixed_programme * bool (* let x = A in B  : (x,A,B) *)
+    | LetRec of mixed_programme * mixed_programme * mixed_programme * bool
+    | Function_def of mixed_programme * mixed_programme * bool  (* fun x -> A  :  (x,A) *)
+    | Function_call of mixed_programme * mixed_programme * bool (* f A : (f,A) *)
+    | IfThenElse of mixed_programme * mixed_programme * mixed_programme * bool (* if x then A else B : (x,A,B) *)
+    | TryWith of mixed_programme * mixed_programme * mixed_programme * bool (* try A with E x -> B : (A,x,B) *)
+    | Raise of mixed_programme * bool
+    | Imp of mixed_programme * mixed_programme * bool (* A;B *)
+    | Ref of mixed_programme * bool (* ref A *)
+    | Bang of mixed_programme * bool  (* !x *)
+    | Assign of mixed_programme * mixed_programme * bool (* x := A *)
+    | AMake of mixed_programme * bool
+    | Affect of mixed_programme * mixed_programme * mixed_programme * bool (* t.(i) <- x : (t,i,x) *)
+    | Access of mixed_programme * mixed_programme * bool (* t.(i)  : (t,i) *)
+
 
     in let mixed = label_pure_code prg in
      let v,b =  (exec_aux (Env.create 10) mixed) in
